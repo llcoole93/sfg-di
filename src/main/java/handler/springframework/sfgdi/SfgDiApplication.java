51f08467dbc2924fff4409821fc8f0777764ed35
Mortimus <llcoole93@gmail.com>
2021-03-31 14:20:03 -0400

    Adding main

---
+++ src/main/java/handler/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,42 @@
+package handler.springframework.sfgdi;
+
+import handler.springframework.sfgdi.controllers.ConstructorInjectedController;
+import handler.springframework.sfgdi.controllers.MyController;
+import handler.springframework.sfgdi.controllers.PropertyInjectedController;
+import handler.springframework.sfgdi.controllers.SetterInjectedController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Properties;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+        MyController myController = (MyController) ctx.getBean("myController");
+
+        System.out.println("------ primary");
+        System.out.println(myController.sayHi());
+
+        System.out.println("----- property");
+        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+        System.out.println(propertyInjectedController.getGreeting());
+
+        System.out.println("----- Setter");
+        SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+        System.out.println(setterInjectedController.getGreetings());
+
+        System.out.println("----- constructor");
+        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+        System.out.println(constructorInjectedController.getGreeting());
+
+
+
+
+
+    }
+
+}
