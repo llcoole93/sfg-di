f3083cce388720e610f7b9dc5a5d4c7d76b22ae2
Mortimus <llcoole93@gmail.com>
2021-03-31 14:42:08 -0400

    default Profile

--- src/main/java/handler/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/handler/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by Evan on 03 / 2021
  */
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingsService {
     @Override
